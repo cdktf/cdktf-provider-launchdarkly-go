a35588be1ac7ae556ebfd4c9c1afeb893a102a48
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-02-18 03:44:31 +0000

    chore(release): v4.6.1

--- launchdarkly/auditlogsubscription/AuditLogSubscriptionConfig.go
+++ launchdarkly/auditlogsubscription/AuditLogSubscriptionConfig.go
@@ -26,34 +26,34 @@ type AuditLogSubscriptionConfig struct {
 	//
 	// Refer to the `formVariables` field in the corresponding `integrations/<integration_key>/manifest.json` file in [this repo](https://github.com/launchdarkly/integration-framework/tree/master/integrations) for a full list of fields for the integration you wish to configure. **IMPORTANT**: Please note that Terraform will only accept these in snake case, regardless of the case shown in the manifest.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#config AuditLogSubscription#config}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#config AuditLogSubscription#config}
 	Config *map[string]*string `field:"required" json:"config" yaml:"config"`
 	// The integration key.
 	//
 	// Supported integration keys are `chronosphere`, `cloudtrail`, `datadog`, `dynatrace`, `elastic`, `grafana`, `honeycomb`, `kosli`, `last9`, `logdna`, `msteams`, `new-relic-apm`, `signalfx`, `slack`, and `splunk`. A change in this field will force the destruction of the existing resource and the creation of a new one.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#integration_key AuditLogSubscription#integration_key}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#integration_key AuditLogSubscription#integration_key}
 	IntegrationKey *string `field:"required" json:"integrationKey" yaml:"integrationKey"`
 	// A human-friendly name for your audit log subscription viewable from within the LaunchDarkly Integrations page.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#name AuditLogSubscription#name}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#name AuditLogSubscription#name}
 	Name *string `field:"required" json:"name" yaml:"name"`
 	// Whether or not you want your subscription enabled, i.e. to actively send events.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#on AuditLogSubscription#on}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#on AuditLogSubscription#on}
 	On interface{} `field:"required" json:"on" yaml:"on"`
 	// statements block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#statements AuditLogSubscription#statements}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#statements AuditLogSubscription#statements}
 	Statements interface{} `field:"required" json:"statements" yaml:"statements"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#id AuditLogSubscription#id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#id AuditLogSubscription#id}.
 	//
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
 	Id *string `field:"optional" json:"id" yaml:"id"`
 	// Tags associated with your resource.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/resources/audit_log_subscription#tags AuditLogSubscription#tags}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/resources/audit_log_subscription#tags AuditLogSubscription#tags}
 	Tags *[]*string `field:"optional" json:"tags" yaml:"tags"`
 }
 
--- launchdarkly/datalaunchdarklyteammember/DataLaunchdarklyTeamMemberRoleAttributes.go
+++ launchdarkly/datalaunchdarklyteammember/DataLaunchdarklyTeamMemberRoleAttributes.go
@@ -7,13 +7,13 @@ package datalaunchdarklyteammember
 type DataLaunchdarklyTeamMemberRoleAttributes struct {
 	// The key / name of your role attribute. In the example `$${roleAttribute/testAttribute}`, the key is `testAttribute`.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/data-sources/team_member#key DataLaunchdarklyTeamMember#key}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/data-sources/team_member#key DataLaunchdarklyTeamMember#key}
 	Key *string `field:"required" json:"key" yaml:"key"`
 	// A list of values for your role attribute.
 	//
 	// For example, if your policy statement defines the resource `"proj/$${roleAttribute/testAttribute}"`, the values would be the keys of the projects you wanted to assign access to.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.0/docs/data-sources/team_member#values DataLaunchdarklyTeamMember#values}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.23.1/docs/data-sources/team_member#values DataLaunchdarklyTeamMember#values}
 	Values *[]*string `field:"required" json:"values" yaml:"values"`
 }
 
